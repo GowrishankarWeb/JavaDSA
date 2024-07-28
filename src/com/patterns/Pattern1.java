80692a1cbb59db4aaddd059c298cf9d4ccdd8f9b
Gowrishankar <96763224+GowrishankarWeb@users.noreply.github.com>
2024-07-28 10:19:43 +0530

    Add files via upload

---
+++ src/com/patterns/Pattern1.java
@@ -0,0 +1,17 @@
+package com.patterns;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+	public static void main(String[] args) {
+		Scanner ip=new Scanner(System.in);
+		int n= ip.nextInt();
+		ip.close();
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
